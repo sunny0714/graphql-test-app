9827b038c37f55b9d82cc77e52661f55bcf2bbb2
sunny0714 <sun2020victory@gmail.com>
2022-03-19 22:07:07 -0800

    student model with a custom field

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,24 @@
+const db = require('./db')
 const Query = {
-  test: () => 'Test Success, GraphQL server is up & running !!'
+   //resolver function for greeting
+   greeting:() => {
+      return "hello from  TutorialsPoint !!!"
+   },
+   
+   //resolver function for students returns list
+   students:() => db.students.list(),
+
+   //resolver function for studentbyId
+   studentById:(root,args,context,info) => {
+      //args will contain parameter passed in query
+      return db.students.get(args.id);
+   }
 }
-module.exports = {Query}
+
+const Student = {
+   fullName:(root,args,context,info) => {
+      return root.firstName+":"+root.lastName
+   }
+}
+
+module.exports = {Query,Student}
