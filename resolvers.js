b1203469bd5cbc75fbefd4d851e3ecc27d9b26a4
sunny0714 <sun2020victory@gmail.com>
2022-03-19 22:18:29 -0800

    mutation

--- resolvers.js
+++ resolvers.js
@@ -1,24 +1,16 @@
 const db = require('./db')
+const Mutation = {
+   createStudent:(root,args,context,info) => {
+      return db.students.create({collegeId:args.collegeId,
+      firstName:args.firstName,
+      lastName:args.lastName})
+   }
+}
 const Query = {
-   //resolver function for greeting
-   greeting:() => {
-      return "hello from  TutorialsPoint !!!"
-   },
-   
-   //resolver function for students returns list
-   students:() => db.students.list(),
-
-   //resolver function for studentbyId
+   greeting:() => "hello",
    studentById:(root,args,context,info) => {
-      //args will contain parameter passed in query
       return db.students.get(args.id);
    }
 }
 
-const Student = {
-   fullName:(root,args,context,info) => {
-      return root.firstName+":"+root.lastName
-   }
-}
-
-module.exports = {Query,Student}
+module.exports = {Query,Mutation}
